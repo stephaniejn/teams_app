c5c70c2fcef7d58edb0c1541dab8f30170af3de4
stephaniejn <stephaniejnilles@gmail.com>
2015-01-11 01:20:50 -0800

    Updated files

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,4 +1,43 @@
 class TeamsController < ApplicationController
+
   def index
+    @teams = Team.all
   end
+
+  def new
+    @team = Team.new
+  end
+
+  def edit
+  @team = Team.find(params[:id])
+  end
+
+  def update
+  @team = Team.find(params[:id])
+    @team.update(team_params)
+      redirect_to @team
+  end
+
+  def show
+    @team = Team.find(params[:id])
+  end
+
+  def create
+    @team = Team.new(team_params)
+
+    @team.save
+    redirect_to "/teams"
+  end
+
+  def destroy
+    @team = Team.find(params[:id])
+    @team.destroy
+    redirect_to teams_path
+  end
+
+   private
+    def team_params
+      params.require(:team).permit(:title, :desc, :loc)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get 'teams/index'
 
-  get 'home/index'
+  get '/' => 'home#index'
+
+  resources :teams
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
