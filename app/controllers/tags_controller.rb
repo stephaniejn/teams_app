54500e04b9d991a40dabd93bd6af5ed312936cf8
stephaniejn <stephaniejnilles@gmail.com>
2015-01-15 10:38:20 -0800

    Updated Tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -4,27 +4,19 @@ class TagsController < ApplicationController
   def new
     @tags = Tag.all
     @tag = Tag.new()
+
   end
 
   def index
     @tags = Tag.all
-    @tag = Tag.find_by(params[:id])
-
 
   end
     def destroy
     @tag = Tag.find(params[:id])
-    if(@tag.teams.length == 0)
     @tag.destroy
     redirect_to tags_path
-  else
-    flash[:danger] = "Cannot delete this tag because there are " + @tag.teams.length.to_s + " teams using it"
-    redirect_to tags_path
-  end
 
   end
-
-
 
 def create
   @tag = Tag.create(tag_params)
--- config/routes.rb
+++ config/routes.rb
@@ -13,8 +13,6 @@ Rails.application.routes.draw do
 
   resources :tags, only: [:index, :new, :create, :destroy]
 
-    post 'tags/:id' => 'tags#notdestroy', as: :notdestroy
-
   match '/404', to: 'errors#file_not_found', via: :all
   match '/422', to: 'errors#unprocessable', via: :all
   match '/500', to: 'errors#internal_server_error', via: :all
