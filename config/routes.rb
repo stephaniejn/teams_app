c062f5acc153713c386e168d1a05c93df2028d83
stephaniejn <stephaniejnilles@gmail.com>
2015-01-20 14:15:10 -0800

    updates

--- config/routes.rb
+++ config/routes.rb
@@ -13,9 +13,9 @@ Rails.application.routes.draw do
 
   resources :tags, only: [:index, :new, :create, :destroy]
 
-  match '/404', to: 'errors#file_not_found', via: :all
-  match '/422', to: 'errors#unprocessable', via: :all
-  match '/500', to: 'errors#internal_server_error', via: :all
+  # get '/404', to: 'errors#file_not_found'
+  # get '/422', to: 'errors#unprocessable'
+  # get '/500', to: 'errors#internal_server_error'
 
 
 
