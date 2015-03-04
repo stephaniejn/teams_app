6b34d40f525b3390bea18cb3a7f8521a12f6cbc5
stephaniejn <stephaniejnilles@gmail.com>
2015-03-04 13:29:26 -0800

    Moved tag cloud & updated formatting

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   def index
     @teams = Team.all
     @team = Team.new()
+    @tags = Tag.all
   end
 
 end
--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -11,27 +11,27 @@ class TagsController < ApplicationController
     @tags = Tag.all
 
   end
-    def destroy
+  def destroy
     @tag = Tag.find(params[:id])
     @tag.destroy
     redirect_to tags_path
 
   end
 
-def create
-  @tag = Tag.create(tag_params)
-  @tags = Tag.all
-  if @tag.errors.any?
-    render :new
-  else
-    flash[:success] = "Added"
-    redirect_to tags_path
+  def create
+    @tag = Tag.create(tag_params)
+    @tags = Tag.all
+    if @tag.errors.any?
+      render :new
+    else
+      flash[:success] = "Added"
+      redirect_to tags_path
+    end
   end
-end
 
-private
-def tag_params
-  params.require(:tag).permit(:name)
-end
+  private
+  def tag_params
+    params.require(:tag).permit(:name)
+  end
 
 end
